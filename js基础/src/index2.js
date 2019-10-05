339078107778eb6f8b3cb5d961f59bda2106b6c0
QRQ <qrq2946649659@gmail.com>
2019-10-06 01:35:43 +0800

    update index2.js

--- js基础/src/index2.js
+++ js基础/src/index2.js
@@ -1,3 +1,30 @@
+/**
+ * 
+ * 观察程序的执行顺序
+ */
+
+
+var myPromise = new Promise(function (resolve, reject) {
+    let a = 0;
+    console.log("立即执行")
+    setTimeout(() => {
+        console.log("lalala")
+        resolve("成功的消息") //调用resolve传入值
+    }, 300);
+})
+
+myPromise.then(function (onfulfilled) {
+    console.log(onfulfilled + "--来自回调")
+    console.log("成功执行--then")
+}, function (onrejected) {
+    console.log(onrejected + "--来自回调")
+    console.log("错误执行--then")
+}).catch(function () {
+    console.log("错误执行--catch")
+}).finally(function () {
+    console.log("最终执行")
+})
+
 Promise.reject("Testing static reject").then(function (reason) {
     // 未被调用
     console.log("未被调用的")
